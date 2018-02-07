423d13a9e8e9b0470e891f3dddd83f5a0d4e4d02
unknown <danish.ahmed92@live.com>
2018-02-07 17:17:04 +0100

    mean, variance, sd

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -16,4 +16,23 @@ public class Util {
                 .collect(Collectors.toMap(
                         Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
+
+    public static double mean(Object[] arr) {
+        double sum = 0.0;
+        for(Object i : arr)
+            sum += Double.parseDouble(i.toString());
+        return sum / arr.length;
+    }
+
+    public static double variance(Object[] arr) {
+        double mean = mean(arr);
+        double temp = 0;
+        for(Object i :arr)
+            temp += (Double.parseDouble(i.toString()) - mean) * (Double.parseDouble(i.toString()) - mean);
+        return temp / (arr.length - 1);
+    }
+
+    public static double standardDeviation(Object[] arr) {
+        return Math.sqrt(variance(arr));
+    }
 }
